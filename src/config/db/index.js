0a5d73c1891409c2be67c38d17fa3f4e379dcc30
Huu Tien <59753913+ChuHuuTien@users.noreply.github.com>
2023-04-07 11:51:08 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,5 @@
 const {mongoose,MongoClient } = require('mongoose');
-const URL = 'mongodb+srv://chuhuutien:0369272144@courses.gpm75af.mongodb.net/courses-blog';
+const URL = 'mongodb+srv://chuhuutien:B1uMYYLnm5npOMrV@courses.gpm75af.mongodb.net/courses-blog';
 const local = 'mongodb://localhost:27017';
 
 async function connect() {
